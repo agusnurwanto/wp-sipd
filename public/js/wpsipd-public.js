7145f965e95bd9ada0aefbc10439bd16daa8cdb2
Idhar Firmansyah <interisty91@gmail.com>
2021-02-18 14:21:50 +0700

    fix perbaikan indikator

--- public/js/wpsipd-public.js
+++ public/js/wpsipd-public.js
@@ -1,32 +1,111 @@
-(function( $ ) {
-	'use strict';
+function run_download_excel(){
+	var current_url = window.location.href;
+	var download_excel = ''
+		+'<div id="action-sipd" class="hide-print">'
+			+'<a id="excel" onclick="return false;" href="#" class="button button-primary">DOWNLOAD EXCEL</a>'
+		+'</div>';
+	jQuery('body').prepend(download_excel);
 
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
+	var style = '';
 
-})( jQuery );
+	style = jQuery('.cetak').attr('style');
+	if (typeof style == 'undefined'){ style = ''; };
+	jQuery('.cetak').attr('style', style+" font-family:'Open Sans',-apple-system,BlinkMacSystemFont,'Segoe UI',sans-serif; padding:0; margin:0; font-size:13px;");
+	
+	jQuery('.bawah').map(function(i, b){
+		style = jQuery(b).attr('style');
+		if (typeof style == 'undefined'){ style = ''; };
+		jQuery(b).attr('style', style+" border-bottom:1px solid #000;");
+	});
+	
+	jQuery('.kiri').map(function(i, b){
+		style = jQuery(b).attr('style');
+		if (typeof style == 'undefined'){ style = ''; };
+		jQuery(b).attr('style', style+" border-left:1px solid #000;");
+	});
+
+	jQuery('.kanan').map(function(i, b){
+		style = jQuery(b).attr('style');
+		if (typeof style == 'undefined'){ style = ''; };
+		jQuery(b).attr('style', style+" border-right:1px solid #000;");
+	});
+
+	jQuery('.atas').map(function(i, b){
+		style = jQuery(b).attr('style');
+		if (typeof style == 'undefined'){ style = ''; };
+		jQuery(b).attr('style', style+" border-top:1px solid #000;");
+	});
+
+	jQuery('.text_tengah').map(function(i, b){
+		style = jQuery(b).attr('style');
+		if (typeof style == 'undefined'){ style = ''; };
+		jQuery(b).attr('style', style+" text-align: center;");
+	});
+
+	jQuery('.text_kiri').map(function(i, b){
+		style = jQuery(b).attr('style');
+		if (typeof style == 'undefined'){ style = ''; };
+		jQuery(b).attr('style', style+" text-align: left;");
+	});
+
+	jQuery('.text_kanan').map(function(i, b){
+		style = jQuery(b).attr('style');
+		if (typeof style == 'undefined'){ style = ''; };
+		jQuery(b).attr('style', style+" text-align: right;");
+	});
+
+	jQuery('.text_block').map(function(i, b){
+		style = jQuery(b).attr('style');
+		if (typeof style == 'undefined'){ style = ''; };
+		jQuery(b).attr('style', style+" font-weight: bold;");
+	});
+
+	jQuery('.text_15').map(function(i, b){
+		style = jQuery(b).attr('style');
+		if (typeof style == 'undefined'){ style = ''; };
+		jQuery(b).attr('style', style+" font-size: 15px;");
+	});
+
+	jQuery('.text_20').map(function(i, b){
+		style = jQuery(b).attr('style');
+		if (typeof style == 'undefined'){ style = ''; };
+		jQuery(b).attr('style', style+" font-size: 20px;");
+	});
+
+	jQuery('td').map(function(i, b){
+		style = jQuery(b).attr('style');
+		if (typeof style == 'undefined'){ style = ''; };
+		jQuery(b).attr('style', style+' mso-number-format:\\@;');
+	});
+
+	jQuery('#excel').on('click', function(){
+		var name = "Laporan";
+		tableHtmlToExcel('cetak', name);
+	});
+}
+
+function tableHtmlToExcel(tableID, filename = ''){
+    var downloadLink;
+    var dataType = 'application/vnd.ms-excel';
+    var tableSelect = document.getElementById(tableID);
+    var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20').replace(/#/g, '%23');
+   
+    filename = filename?filename+'.xls':'excel_data.xls';
+   
+    downloadLink = document.createElement("a");
+    
+    document.body.appendChild(downloadLink);
+    
+    if(navigator.msSaveOrOpenBlob){
+        var blob = new Blob(['\ufeff', tableHTML], {
+            type: dataType
+        });
+        navigator.msSaveOrOpenBlob( blob, filename);
+    }else{
+        downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+   
+        downloadLink.download = filename;
+       
+        downloadLink.click();
+    }
+}
--- public/partials/wpsipd-public-rkpd.php
+++ public/partials/wpsipd-public-rkpd.php
@@ -28,89 +28,17 @@ if(!empty($input['id_skpd'])){
 $units = $wpdb->get_results($sql, ARRAY_A);
 if(empty($units)){
 	die('<h1>SKPD tidak ditemukan!</h1>');
+}else{
+	$pengaturan = $wpdb->get_results($wpdb->prepare("
+		select 
+			* 
+		from data_pengaturan_sipd 
+		where tahun_anggaran=%d
+	", $input['tahun_anggaran']), ARRAY_A);
 }
-
 ?>
-<style type="text/css">
-    .cetak{
-        font-family:'Open Sans',-apple-system,BlinkMacSystemFont,'Segoe UI',sans-serif;
-        padding:0;
-        margin:0;
-        font-size:11pt;
-    }
-    .m-t-20{
-        margin-top:20px;
-    }
-    .bawah{
-        border-bottom:1px solid #000;        
-    }
-    .kiri{
-        border-left:1px solid #000;        
-    }
-    .kanan{        
-        border-right:1px solid #000;
-    }
-    .atas{        
-        border-top:1px solid #000;
-    }
-    .text_tengah{
-        text-align: center;
-    }
-    .text_kiri{
-        text-align: left;
-    }
-    .text_kanan{
-        text-align: right;
-    }
-    .text_blok{
-        font-weight: bold;
-    }        
-    .text_15{
-        font-size: 15px;
-    }
-    .text_20{
-        font-size: 20px;
-    }
-    .logoprinter{
-        position:absolute;
-        top:150px;
-        right:600px;
-    }
-    thead{
-        display:table-header-group;
-    }
-    .setbreakafter{
-        page-break-after: always;
-    }
-    .setbreakbefore{
-        page-break-before: always;
-    }
-    td {
-        page-break-inside: avoid;
-    }
-    @page  {
-        size: A3 landscape;
-    }
-    @media  print {
-        #form-table{
-            font-size: 73%;
-        }
-        body {
-            padding:0px;
-        }
-    }
-    @media  screen {
-        #form-table{
-            font-size: 60%;
-        }
-        body {
-            padding:3px;
-        }        
-    }
-</style>
-
-<h4 style="text-align: center; font-size: 11px; margin: 0; font-weight: bold;">Program dan Kegiatan Perangkat Daerah<br/>Kabupaten Magetan <br/>Tahun <?php echo $input['tahun_anggaran']; ?></h4>
-
+<div id="cetak">
+	<h4 style="text-align: center; font-size: 11px; margin: 0; font-weight: bold;">Program dan Kegiatan Perangkat Daerah<br/>Kabupaten Magetan <br/>Tahun <?php echo $input['tahun_anggaran']; ?></h4>
 <?php 
 	foreach ($units as $k => $unit): 
 		if($unit['is_skpd']==1){
@@ -133,7 +61,7 @@ if(empty($units)){
 			where tahun_anggaran=%d
 				and active=1
 				and id_skpd=%d
-			order by id_sub_giat ASC
+			order by kode_sub_giat ASC
 		", $input['tahun_anggaran'], $unit['id_skpd']), ARRAY_A);
 
 		$data_all = array(
@@ -142,6 +70,60 @@ if(empty($units)){
 			'data' => array()
 		);
 		foreach ($subkeg as $kk => $sub) {
+			$capaian_prog = $wpdb->get_results($wpdb->prepare("
+				select 
+					* 
+				from data_capaian_prog_sub_keg 
+				where tahun_anggaran=%d
+					and active=1
+					and kode_sbl=%s
+				order by id ASC
+			", $input['tahun_anggaran'], $sub['kode_sbl']), ARRAY_A);
+
+			$output_giat = $wpdb->get_results($wpdb->prepare("
+				select 
+					* 
+				from data_output_giat_sub_keg 
+				where tahun_anggaran=%d
+					and active=1
+					and kode_sbl=%s
+				order by id ASC
+			", $input['tahun_anggaran'], $sub['kode_sbl']), ARRAY_A);
+
+			$output_sub_giat = $wpdb->get_results($wpdb->prepare("
+				select 
+					* 
+				from data_sub_keg_indikator
+				where tahun_anggaran=%d
+					and active=1
+					and kode_sbl=%s
+				order by id ASC
+			", $input['tahun_anggaran'], $sub['kode_sbl']), ARRAY_A);
+
+			$lokasi_sub_giat = $wpdb->get_results($wpdb->prepare("
+				select 
+					* 
+				from data_lokasi_sub_keg
+				where tahun_anggaran=%d
+					and active=1
+					and kode_sbl=%s
+				order by id ASC
+			", $input['tahun_anggaran'], $sub['kode_sbl']), ARRAY_A);
+
+			$nama = explode(' ', $sub['nama_sub_giat']);
+			$kode_sub_giat = $nama[0];
+			$data_renstra = $wpdb->get_results($wpdb->prepare("
+				select 
+					* 
+				from data_renstra
+				where tahun_anggaran=%d
+					and active=1
+					and kode_sub_giat=%s
+					and kode_skpd=%s
+				order by id ASC
+			", $input['tahun_anggaran'], $kode_sub_giat, $unit['kode_skpd']), ARRAY_A);
+			// die($wpdb->last_query);
+
 			if(empty($data_all['data'][$sub['kode_urusan']])){
 				$data_all['data'][$sub['kode_urusan']] = array(
 					'nama'	=> $sub['nama_urusan'],
@@ -181,6 +163,11 @@ if(empty($units)){
 					'nama'	=> implode(' ', $nama),
 					'total' => 0,
 					'total_n_plus' => 0,
+					'capaian_prog' => $capaian_prog,
+					'output_giat' => $output_giat,
+					'output_sub_giat' => $output_sub_giat,
+					'lokasi_sub_giat' => $lokasi_sub_giat,
+					'data_renstra' => $data_renstra,
 					'data'	=> array()
 				);
 			}
@@ -191,12 +178,12 @@ if(empty($units)){
 			$data_all['data'][$sub['kode_urusan']]['data'][$sub['kode_bidang_urusan']]['data'][$sub['kode_program']]['data'][$sub['kode_giat']]['total'] += $sub['pagu'];
 			$data_all['data'][$sub['kode_urusan']]['data'][$sub['kode_bidang_urusan']]['data'][$sub['kode_program']]['data'][$sub['kode_giat']]['data'][$sub['kode_sub_giat']]['total'] += $sub['pagu'];
 
-			$data_all['total_n_plus'] += $sub['pagu'];
-			$data_all['data'][$sub['kode_urusan']]['total_n_plus'] += $sub['pagu'];
-			$data_all['data'][$sub['kode_urusan']]['data'][$sub['kode_bidang_urusan']]['total_n_plus'] += $sub['pagu'];
-			$data_all['data'][$sub['kode_urusan']]['data'][$sub['kode_bidang_urusan']]['data'][$sub['kode_program']]['total_n_plus'] += $sub['pagu'];
-			$data_all['data'][$sub['kode_urusan']]['data'][$sub['kode_bidang_urusan']]['data'][$sub['kode_program']]['data'][$sub['kode_giat']]['total_n_plus'] += $sub['pagu'];
-			$data_all['data'][$sub['kode_urusan']]['data'][$sub['kode_bidang_urusan']]['data'][$sub['kode_program']]['data'][$sub['kode_giat']]['data'][$sub['kode_sub_giat']]['total_n_plus'] += $sub['pagu'];
+			$data_all['total_n_plus'] += $sub['pagu_n_depan'];
+			$data_all['data'][$sub['kode_urusan']]['total_n_plus'] += $sub['pagu_n_depan'];
+			$data_all['data'][$sub['kode_urusan']]['data'][$sub['kode_bidang_urusan']]['total_n_plus'] += $sub['pagu_n_depan'];
+			$data_all['data'][$sub['kode_urusan']]['data'][$sub['kode_bidang_urusan']]['data'][$sub['kode_program']]['total_n_plus'] += $sub['pagu_n_depan'];
+			$data_all['data'][$sub['kode_urusan']]['data'][$sub['kode_bidang_urusan']]['data'][$sub['kode_program']]['data'][$sub['kode_giat']]['total_n_plus'] += $sub['pagu_n_depan'];
+			$data_all['data'][$sub['kode_urusan']]['data'][$sub['kode_bidang_urusan']]['data'][$sub['kode_program']]['data'][$sub['kode_giat']]['data'][$sub['kode_sub_giat']]['total_n_plus'] += $sub['pagu_n_depan'];
 		}
 
 		// print_r($data_all); die();
@@ -263,6 +250,47 @@ if(empty($units)){
 						foreach ($giat['data'] as $kd_sub_giat => $sub_giat) {
 							$kd_sub_giat = explode('.', $kd_sub_giat);
 							$kd_sub_giat = $kd_sub_giat[count($kd_sub_giat)-1];
+							$capaian_prog = '';
+							if(!empty($sub_giat['capaian_prog'])){
+								$capaian_prog = $sub_giat['capaian_prog'][0]['capaianteks'];
+							}
+							$target_capaian_prog = '';
+							if(!empty($sub_giat['capaian_prog'])){
+								$target_capaian_prog = $sub_giat['capaian_prog'][0]['targetcapaianteks'];
+							}
+							$output_giat = '';
+							if(!empty($sub_giat['output_giat'])){
+								$output_giat = $sub_giat['output_giat'][0]['outputteks'];
+							}
+							$target_output_giat = '';
+							if(!empty($sub_giat['output_giat'])){
+								$target_output_giat = $sub_giat['output_giat'][0]['targetoutputteks'];
+							}
+							$output_sub_giat = '';
+							if(!empty($sub_giat['output_sub_giat'])){
+								$output_sub_giat = $sub_giat['output_sub_giat'][0]['outputteks'];
+							}
+							$target_output_sub_giat = '';
+							if(!empty($sub_giat['output_sub_giat'])){
+								$target_output_sub_giat = $sub_giat['output_sub_giat'][0]['targetoutputteks'];
+							}
+							$lokasi_sub_giat = '';
+							if(!empty($sub_giat['lokasi_sub_giat'])){
+								$lokasi_sub_giat = $sub_giat['lokasi_sub_giat'][0]['daerahteks'].', '.$sub_giat['lokasi_sub_giat'][0]['camatteks'].', '.$sub_giat['lokasi_sub_giat'][0]['lurahteks'];
+							}
+							$ind_n_plus = '';
+							$target_ind_n_plus = '';
+							if(!empty($sub_giat['data_renstra'])){
+								$ind_n_plus = $sub_giat['data_renstra'][0]['indikator_sub'];
+								$start_rpjmd = 2018;
+								if(!empty($pengaturan)){
+									$start_rpjmd = $pengaturan[0]['awal_rpjmd'];
+								}
+								$urut = $input['tahun_anggaran']-$start_rpjmd;
+								if($urut<=5){
+									$target_ind_n_plus = $sub_giat['data_renstra'][0]['target_sub_'.($urut+1)];
+								}
+							}
 							$body .= '
 						        <tr>
 						            <td class="kiri kanan bawah">'.$kd_urusan.'</td>
@@ -271,18 +299,18 @@ if(empty($units)){
 						            <td class="kanan bawah">'.$kd_giat.'</td>
 						            <td class="kanan bawah">'.$kd_sub_giat.'</td>
 						            <td class="kanan bawah">'.$sub_giat['nama'].'</td>
-						            <td class="kanan bawah">capaian program</td>
-						            <td class="kanan bawah">indikator kegiatan</td>
-						            <td class="kanan bawah">hasil kegiatan</td>
-						            <td class="kanan bawah">lokasi output kegiatan</td>
-						            <td class="kanan bawah">target program</td>
-						            <td class="kanan bawah">target sub giat</td>
-						            <td class="kanan bawah">target hasil giat</td>
+						            <td class="kanan bawah">'.$capaian_prog.'</td>
+						            <td class="kanan bawah">'.$output_sub_giat.'</td>
+						            <td class="kanan bawah">'.$output_giat.'</td>
+						            <td class="kanan bawah">'.$lokasi_sub_giat.'</td>
+						            <td class="kanan bawah">'.$target_capaian_prog.'</td>
+						            <td class="kanan bawah">'.$target_output_sub_giat.'</td>
+						            <td class="kanan bawah">'.$target_output_giat.'</td>
 						            <td class="kanan bawah text_kanan">'.number_format($sub_giat['total'],0,",",".").'</td>
 						            <td class="kanan bawah"><br/></td>
 						            <td class="kanan bawah">&nbsp;</td>
-						            <td class="kanan bawah">tolak ukur n+1</td>
-						            <td class="kanan bawah">target n+1</td>
+						            <td class="kanan bawah">'.$ind_n_plus.'</td>
+						            <td class="kanan bawah">'.$target_ind_n_plus.'</td>
 						            <td class="kanan bawah text_kanan">'.number_format($sub_giat['total_n_plus'],0,",",".").'</td>
 						        </tr>
 							';
@@ -292,70 +320,74 @@ if(empty($units)){
 			}
 		}
 ?>
-<table id="form-table" cellpadding="2" cellspacing="0" style="font-family:'Open Sans',-apple-system,BlinkMacSystemFont,'Segoe UI',sans-serif; border-collapse: collapse; width:100%; table-layout:fixed; overflow-wrap: break-word; font-size: 60%; border: 0;">
-    <thead>
-    	<tr>
-            <th style="padding: 0; border: 0; width:1%"></th>
-            <th style="padding: 0; border: 0; width:1.2%"></th>
-            <th style="padding: 0; border: 0; width:1.5%"></th>
-            <th style="padding: 0; border: 0; width:1.5%"></th>
-            <th style="padding: 0; border: 0; width:1.5%"></th>
-            <th style="padding: 0; border: 0; width:7%"></th>
-            <th style="padding: 0; border: 0; width:7.5%"></th>
-            <th style="padding: 0; border: 0; width:7.5%"></th>
-            <th style="padding: 0; border: 0; width:7.5%"></th>
-            <th style="padding: 0; border: 0; width:4%"></th>
-            <th style="padding: 0; border: 0; width:3.5%"></th>
-            <th style="padding: 0; border: 0; width:3.5%"></th>
-            <th style="padding: 0; border: 0; width:3.5%"></th>
-            <th style="padding: 0; border: 0; width:5.5%"></th>
-            <th style="padding: 0; border: 0; width:3.5%"></th>
-            <th style="padding: 0; border: 0; width:4%"></th>
-            <th style="padding: 0; border: 0; width:7.5%"></th>
-            <th style="padding: 0; border: 0; width:3.5%"></th>
-            <th style="padding: 0; border: 0; width:5.5%"></th>
-        </tr>
-	    <tr>
-	        <td colspan="19" style="vertical-align:middle; font-weight:bold; border: 0;">
-	            Unit Organisasi : <?php echo $unit_induk[0]['kode_skpd']; ?>&nbsp;<?php echo $unit_induk[0]['nama_skpd']; ?><br/>
-	            Sub Unit Organisasi : <?php echo $unit['kode_skpd']; ?>&nbsp;<?php echo $unit['nama_skpd']; ?>
-	        </td>
-	    </tr>
-	    <tr>
-	        <td class="atas kanan bawah kiri text_tengah text_blok" colspan="5" rowspan="3">Kode</td>
-	        <td class="atas kanan bawah text_tengah text_blok" rowspan="3">Urusan/ Bidang Urusan Pemerintahan Daerah Dan Program/ Kegiatan</td>
-	        <td class="atas kanan bawah text_tengah text_blok" colspan="3">Indikator Kinerja</td>
-	        <td class="atas kanan bawah text_tengah text_blok" colspan="6">Rencana Tahun <?php echo $input['tahun_anggaran']; ?></td>
-	        <td class="atas kanan bawah text_tengah text_blok" rowspan="3">Catatan Penting</td>
-	        <td class="atas kanan bawah text_tengah text_blok" colspan="3">Prakiraan Maju Rencana Tahun <?php echo ($input['tahun_anggaran']+1); ?></td>
-	    </tr>
-	    <tr>
-	        <td class="kanan bawah text_tengah text_blok" rowspan="2">Capaian Program</td>
-	        <td class="kanan bawah text_tengah text_blok" rowspan="2">Keluaran Sub Kegiatan</td>
-	        <td class="kanan bawah text_tengah text_blok" rowspan="2">Hasil Kegiatan</td>
-	        <td class="kanan bawah text_tengah text_blok" rowspan="2">Lokasi Output Kegiatan</td>
-	        <td class="kanan bawah text_tengah text_blok" colspan="3">Target Capaian Kinerja</td>
-	        <td class="kanan bawah text_tengah text_blok" rowspan="2">Pagu Indikatif (Rp.)</td>
-	        <td class="kanan bawah text_tengah text_blok" rowspan="2">Sumber Dana</td>
-	        <td class="kanan bawah text_tengah text_blok" colspan="2">Target Capaian Kinerja</td>
-	        <td class="kanan bawah text_tengah text_blok" rowspan="2">Kebutuhan Dana/<br/>Pagu Indikatif (Rp.)</td>
-	    </tr>
-	    <tr>
-	        <td class="kanan bawah text_tengah text_blok">Program</td>
-	        <td class="kanan bawah text_tengah text_blok">Keluaran Sub Kegiatan</td>
-	        <td class="kanan bawah text_tengah text_blok">Hasil Kegiatan</td>
-	        <td class="kanan bawah text_tengah text_blok">Tolok Ukur</td>
-	        <td class="kanan bawah text_tengah text_blok">Target</td>
-	    </tr>
-    </thead>
-    <tbody>
-        <?php echo $body; ?>
-		<tr>
-	        <td class="kiri kanan bawah text_blok text_kanan" colspan="13">TOTAL</td>
-	        <td class="kanan bawah text_kanan text_blok"><?php echo number_format($data_all['total'],0,",","."); ?></td>
-	        <td class="kanan bawah" colspan="4">&nbsp;</td>
-	        <td class="kanan bawah text_kanan text_blok"><?php echo number_format($data_all['total_n_plus'],0,",","."); ?></td>
-	    </tr>
-    </tbody>
-</table>
+	<table cellpadding="2" cellspacing="0" style="font-family:'Open Sans',-apple-system,BlinkMacSystemFont,'Segoe UI',sans-serif; border-collapse: collapse; width:100%; table-layout:fixed; overflow-wrap: break-word; font-size: 60%; border: 0;">
+	    <thead>
+	    	<tr>
+	            <th style="padding: 0; border: 0; width:1%"></th>
+	            <th style="padding: 0; border: 0; width:1.2%"></th>
+	            <th style="padding: 0; border: 0; width:1.5%"></th>
+	            <th style="padding: 0; border: 0; width:1.5%"></th>
+	            <th style="padding: 0; border: 0; width:1.5%"></th>
+	            <th style="padding: 0; border: 0; width:7%"></th>
+	            <th style="padding: 0; border: 0; width:7.5%"></th>
+	            <th style="padding: 0; border: 0; width:7.5%"></th>
+	            <th style="padding: 0; border: 0; width:7.5%"></th>
+	            <th style="padding: 0; border: 0; width:4%"></th>
+	            <th style="padding: 0; border: 0; width:3.5%"></th>
+	            <th style="padding: 0; border: 0; width:3.5%"></th>
+	            <th style="padding: 0; border: 0; width:3.5%"></th>
+	            <th style="padding: 0; border: 0; width:5.5%"></th>
+	            <th style="padding: 0; border: 0; width:3.5%"></th>
+	            <th style="padding: 0; border: 0; width:4%"></th>
+	            <th style="padding: 0; border: 0; width:7.5%"></th>
+	            <th style="padding: 0; border: 0; width:3.5%"></th>
+	            <th style="padding: 0; border: 0; width:5.5%"></th>
+	        </tr>
+		    <tr>
+		        <td colspan="19" style="vertical-align:middle; font-weight:bold; border: 0;">
+		            Unit Organisasi : <?php echo $unit_induk[0]['kode_skpd']; ?>&nbsp;<?php echo $unit_induk[0]['nama_skpd']; ?><br/>
+		            Sub Unit Organisasi : <?php echo $unit['kode_skpd']; ?>&nbsp;<?php echo $unit['nama_skpd']; ?>
+		        </td>
+		    </tr>
+		    <tr>
+		        <td class="atas kanan bawah kiri text_tengah text_blok" colspan="5" rowspan="3">Kode</td>
+		        <td class="atas kanan bawah text_tengah text_blok" rowspan="3">Urusan/ Bidang Urusan Pemerintahan Daerah Dan Program/ Kegiatan</td>
+		        <td class="atas kanan bawah text_tengah text_blok" colspan="3">Indikator Kinerja</td>
+		        <td class="atas kanan bawah text_tengah text_blok" colspan="6">Rencana Tahun <?php echo $input['tahun_anggaran']; ?></td>
+		        <td class="atas kanan bawah text_tengah text_blok" rowspan="3">Catatan Penting</td>
+		        <td class="atas kanan bawah text_tengah text_blok" colspan="3">Prakiraan Maju Rencana Tahun <?php echo ($input['tahun_anggaran']+1); ?></td>
+		    </tr>
+		    <tr>
+		        <td class="kanan bawah text_tengah text_blok" rowspan="2">Capaian Program</td>
+		        <td class="kanan bawah text_tengah text_blok" rowspan="2">Keluaran Sub Kegiatan</td>
+		        <td class="kanan bawah text_tengah text_blok" rowspan="2">Hasil Kegiatan</td>
+		        <td class="kanan bawah text_tengah text_blok" rowspan="2">Lokasi Output Kegiatan</td>
+		        <td class="kanan bawah text_tengah text_blok" colspan="3">Target Capaian Kinerja</td>
+		        <td class="kanan bawah text_tengah text_blok" rowspan="2">Pagu Indikatif (Rp.)</td>
+		        <td class="kanan bawah text_tengah text_blok" rowspan="2">Sumber Dana</td>
+		        <td class="kanan bawah text_tengah text_blok" colspan="2">Target Capaian Kinerja</td>
+		        <td class="kanan bawah text_tengah text_blok" rowspan="2">Kebutuhan Dana/<br/>Pagu Indikatif (Rp.)</td>
+		    </tr>
+		    <tr>
+		        <td class="kanan bawah text_tengah text_blok">Program</td>
+		        <td class="kanan bawah text_tengah text_blok">Keluaran Sub Kegiatan</td>
+		        <td class="kanan bawah text_tengah text_blok">Hasil Kegiatan</td>
+		        <td class="kanan bawah text_tengah text_blok">Tolok Ukur</td>
+		        <td class="kanan bawah text_tengah text_blok">Target</td>
+		    </tr>
+	    </thead>
+	    <tbody>
+	        <?php echo $body; ?>
+			<tr>
+		        <td class="kiri kanan bawah text_blok text_kanan" colspan="13">TOTAL</td>
+		        <td class="kanan bawah text_kanan text_blok"><?php echo number_format($data_all['total'],0,",","."); ?></td>
+		        <td class="kanan bawah" colspan="4">&nbsp;</td>
+		        <td class="kanan bawah text_kanan text_blok"><?php echo number_format($data_all['total_n_plus'],0,",","."); ?></td>
+		    </tr>
+	    </tbody>
+	</table>
+</div>
 <?php endforeach; ?>
+<script type="text/javascript">
+	run_download_excel();
+</script>
