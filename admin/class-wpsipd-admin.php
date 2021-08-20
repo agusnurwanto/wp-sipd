c91be97aeb32d3cdfed644b01aafba82a1ee09f9
agusnurwanto <agusnurwantomuslim@gmail.com>
2021-08-20 14:51:00 +0700

    perbaikan shorting daftar SKPD

--- admin/class-wpsipd-admin.php
+++ admin/class-wpsipd-admin.php
@@ -192,8 +192,15 @@ class Wpsipd_Admin {
             	->set_html( '<a target="_blank" href="'.$sinergi_link.'">SIPD to SINERGI DJPK Kementrian Keuangan</a>' ),*/
         );
 
-		$rfk_pemda = array();
-		$monev_pemda = array();
+		$hide_sidebar = Field::make( 'html', 'crb_hide_sidebar' )
+        	->set_html( '
+        		<style>
+        			.postbox-container { display: none; }
+        			#poststuff #post-body.columns-2 { margin: 0 !important; }
+        		</style>
+        	' );
+		$rfk_pemda = array($hide_sidebar);
+		$monev_pemda = array($hide_sidebar);
 		$tahun = $wpdb->get_results('select tahun_anggaran from data_unit group by tahun_anggaran order by tahun_anggaran ASC', ARRAY_A);
 		foreach ($tahun as $k => $v) {
 			$url = $this->generatePage('Monitoring Update Data SIPD lokal Berdasar Waktu Terakhir Melakukan Singkronisasi Data | '.$v['tahun_anggaran'], $v['tahun_anggaran']);
@@ -203,20 +210,20 @@ class Wpsipd_Admin {
 			$url_pemda = $this->generatePage('Realisasi Fisik dan Keuangan Pemerintah Daerah | '.$v['tahun_anggaran'], $v['tahun_anggaran'], '[monitor_rfk tahun_anggaran="'.$v['tahun_anggaran'].'"]');
 			$url_pemda_monev = $this->generatePage('MONEV RENJA Pemerintah Daerah | '.$v['tahun_anggaran'], $v['tahun_anggaran'], '[monitor_monev_renja tahun_anggaran="'.$v['tahun_anggaran'].'"]');
 
-            $unit = $wpdb->get_results("SELECT nama_skpd, id_skpd, kode_skpd from data_unit where active=1 and tahun_anggaran=".$v['tahun_anggaran'].' and is_skpd=1 order by nama_skpd ASC', ARRAY_A);
+            $unit = $wpdb->get_results("SELECT nama_skpd, id_skpd, kode_skpd from data_unit where active=1 and tahun_anggaran=".$v['tahun_anggaran'].' and is_skpd=1 order by kode_skpd ASC', ARRAY_A);
             $body_pemda = '<ul style="margin-left: 20px;">';
             $body_pemda_monev = '<ul style="margin-left: 20px;">';
             foreach ($unit as $kk => $vv) {
-            	$subunit = $wpdb->get_results("SELECT nama_skpd, id_skpd, kode_skpd from data_unit where active=1 and tahun_anggaran=".$v['tahun_anggaran']." and is_skpd=0 and id_unit=".$vv["id_skpd"]." order by nama_skpd ASC", ARRAY_A);
+            	$subunit = $wpdb->get_results("SELECT nama_skpd, id_skpd, kode_skpd from data_unit where active=1 and tahun_anggaran=".$v['tahun_anggaran']." and is_skpd=0 and id_unit=".$vv["id_skpd"]." order by kode_skpd ASC", ARRAY_A);
 
 				$url_skpd = $this->generatePage('RFK '.$vv['nama_skpd'].' '.$vv['kode_skpd'].' | '.$v['tahun_anggaran'], $v['tahun_anggaran'], '[monitor_rfk tahun_anggaran="'.$v['tahun_anggaran'].'" id_skpd="'.$vv['id_skpd'].'"]');
-            	$body_pemda .= '<li><a target="_blank" href="'.$url_skpd.'">Halaman RFK '.$vv['nama_skpd'].' '.$v['tahun_anggaran'].'</a>';
+            	$body_pemda .= '<li><a target="_blank" href="'.$url_skpd.'">Halaman RFK '.$vv['kode_skpd'].' '.$vv['nama_skpd'].' '.$v['tahun_anggaran'].'</a>';
             	if(!empty($subunit)){
-            		$body_pemda .= '<ul style="margin-left: 20px;">';
+            		$body_pemda .= '<ul style="margin: 5px 20px;">';
             	}
             	foreach ($subunit as $kkk => $vvv) {
 					$url_skpd = $this->generatePage('RFK '.$vvv['nama_skpd'].' '.$vvv['kode_skpd'].' | '.$v['tahun_anggaran'], $v['tahun_anggaran'], '[monitor_rfk tahun_anggaran="'.$v['tahun_anggaran'].'" id_skpd="'.$vvv['id_skpd'].'"]');
-            		$body_pemda .= '<li><a target="_blank" href="'.$url_skpd.'">Halaman RFK '.$vvv['nama_skpd'].' '.$v['tahun_anggaran'].'</a></li>';
+            		$body_pemda .= '<li><a target="_blank" href="'.$url_skpd.'">Halaman RFK '.$vvv['kode_skpd'].' '.$vvv['nama_skpd'].' '.$v['tahun_anggaran'].'</a></li>';
             	}
             	if(!empty($subunit)){
             		$body_pemda .= '</ul>';
@@ -224,13 +231,13 @@ class Wpsipd_Admin {
             	$body_pemda .= '</li>';
 
 				$url_skpd = $this->generatePage('MONEV '.$vv['nama_skpd'].' '.$vv['kode_skpd'].' | '.$v['tahun_anggaran'], $v['tahun_anggaran'], '[monitor_monev_renja tahun_anggaran="'.$v['tahun_anggaran'].'" id_skpd="'.$vv['id_skpd'].'"]');
-            	$body_pemda_monev .= '<li><a target="_blank" href="'.$url_skpd.'">Halaman MONEV '.$vv['nama_skpd'].' '.$v['tahun_anggaran'].'</a>';
+            	$body_pemda_monev .= '<li><a target="_blank" href="'.$url_skpd.'">Halaman MONEV '.$vv['kode_skpd'].' '.$vv['nama_skpd'].' '.$v['tahun_anggaran'].'</a>';
             	if(!empty($subunit)){
-            		$body_pemda_monev .= '<ul style="margin-left: 20px;">';
+            		$body_pemda_monev .= '<ul style="margin: 5px 20px;">';
             	}
             	foreach ($subunit as $kkk => $vvv) {
 					$url_skpd = $this->generatePage('MONEV '.$vvv['nama_skpd'].' '.$vvv['kode_skpd'].' | '.$v['tahun_anggaran'], $v['tahun_anggaran'], '[monitor_monev_renja tahun_anggaran="'.$v['tahun_anggaran'].'" id_skpd="'.$vvv['id_skpd'].'"]');
-            		$body_pemda_monev .= '<li><a target="_blank" href="'.$url_skpd.'">Halaman MONEV '.$vvv['nama_skpd'].' '.$v['tahun_anggaran'].'</a></li>';
+            		$body_pemda_monev .= '<li><a target="_blank" href="'.$url_skpd.'">Halaman MONEV '.$vvv['kode_skpd'].' '.$vvv['nama_skpd'].' '.$v['tahun_anggaran'].'</a></li>';
             	}
             	if(!empty($subunit)){
             		$body_pemda_monev .= '</ul>';
