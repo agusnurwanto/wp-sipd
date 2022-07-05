7d30cb0bcf55dcde6851d3529ca42123704ad7d6
dwisugi <dwisugianto17@gmail.com>
2022-07-05 21:39:34 +0700

    perbaikan validasi tombol aksi

--- public/partials/wpsipd-public-ssh-usulan.php
+++ public/partials/wpsipd-public-ssh-usulan.php
@@ -982,8 +982,12 @@ $body = '';
 		})
 	}
 
-	function cannot_edit_ssh_usulan(){
-		alert('Tidak bisa edit usulan SSH karena sudah ditahap verifikasi');
+	function cannot_change_ssh_usulan(option,jenis){
+		if(jenis == 'usulan'){
+			alert('Tidak bisa '+option+' usulan SSH karena sudah ditahap verifikasi');
+		}else if(jenis == 'upload'){
+			alert('Tidak bisa '+option+' usulan SSH karena sudah ditahap upload SIPD');
+		}
 	}
 
 	function submitEditUsulanSshForm(id_standar_harga,tahun){
