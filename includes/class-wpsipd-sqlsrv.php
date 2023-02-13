b625f6884943e51f5d0df4a3e3cf5e10ad3c14d8
n3f0 <nefo424@gmail.com>
2023-02-13 23:33:58 +0700

    library sql server

--- includes/class-wpsipd-sqlsrv.php
+++ includes/class-wpsipd-sqlsrv.php
@@ -24,25 +24,36 @@ class Wpsipd_sqlsrv{
     }
 
     private function connect(){
-       return new PDO("sqlsrv:Server=".$this->dbhost.",".$this->dbport.";Database=".$this->dbname,$this->dbuser,$this->dbpass);
+        try{
+            $conn new PDO("sqlsrv:Server=".$this->dbhost.",".$this->dbport.";Database=".$this->dbname,$this->dbuser,$this->dbpass);
+            $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+            return $conn;
+        }catch(Exception $e){
+            return null;
+        }
     }
 
+    //query dinamis sql server
     public function query($qr,$param){
 
     }
 
+    //menambah data di table sql server
     public function insert($tbl,$param){
 
     }
 
+    //perbaharui data di table sql server
     public function update($tbl,$param,$where){
 
     }
 
+    //hapus data di table database sql server
     public function delete($tbl,$where){
 
     }
 
+    //exec stored procedure sql server
     public function exec($sp,$param){
 
     }
