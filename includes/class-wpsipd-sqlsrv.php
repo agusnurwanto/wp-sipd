61b36c6580f44567ba1eeb6f0e0c3ba299cc482d
n3f0 <nefo424@gmail.com>
2023-02-13 23:29:39 +0700

    library sql server wp-sipd

--- includes/class-wpsipd-sqlsrv.php
+++ includes/class-wpsipd-sqlsrv.php
@@ -5,16 +5,26 @@
 
 class Wpsipd_sqlsrv{
     private $conn;
+    private $dbhost;
+    private $dbport;
+    private $dbuser;
+    private $dbpass;
+    private $dbname;
     public function __construct($host,$port,$usrdb,$passdb,$dbname){
-
+        $this->dbhost=$host;
+        $this->dbport=$port;
+        $this->dbuser=$usrdb;
+        $this->dbpass=$passdb;
+        $this->dbname=$dbname;
+        $this->conn=$this->connect();
     }
 
     public function __destruct(){
-        
+        $this->conn.close();
     }
 
     private function connect(){
-
+       return new PDO("sqlsrv:Server=".$this->dbhost.",".$this->dbport.";Database=".$this->dbname,$this->dbuser,$this->dbpass);
     }
 
     public function query($qr,$param){
