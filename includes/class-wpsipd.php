fe9534ad9d7d064816ce1815cb6a3748dc95dc9b
Saiful Rahman <saifulbangil@gmail.com>
2023-04-16 03:45:35 +0700

    sigkron master label giat sipd -ri

--- includes/class-wpsipd.php
+++ includes/class-wpsipd.php
@@ -231,6 +231,9 @@ class Wpsipd
 		$this->loader->add_action('wp_ajax_singkron_ssh',  $plugin_public, 'singkron_ssh');
 		$this->loader->add_action('wp_ajax_nopriv_singkron_ssh',  $plugin_public, 'singkron_ssh');
 
+		$this->loader->add_action('wp_ajax_singkron_label_giat',  $plugin_public, 'singkron_label_giat');
+		$this->loader->add_action('wp_ajax_nopriv_singkron_label_giat',  $plugin_public, 'singkron_label_giat');
+
 		$this->loader->add_action('wp_ajax_singkron_label_kokab',  $plugin_public, 'singkron_label_kokab');
 		$this->loader->add_action('wp_ajax_nopriv_singkron_label_kokab',  $plugin_public, 'singkron_label_kokab');
 
