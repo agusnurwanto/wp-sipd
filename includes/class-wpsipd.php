80e592d1d3edd6e50c116fbf82431c9724625cc3
dwisugi <dwisugianto17@gmail.com>
2023-09-19 17:23:54 +0700

    menambahkan fitur halaman analisis belanja per rekening

--- includes/class-wpsipd.php
+++ includes/class-wpsipd.php
@@ -881,6 +881,7 @@ class Wpsipd
 		$this->loader->add_action('wp_ajax_show_skpd_bidang_urusan_analisis',  $plugin_public, 'show_skpd_bidang_urusan_analisis');
 		$this->loader->add_action('wp_ajax_show_skpd_sumber_dana_analisis',  $plugin_public, 'show_skpd_sumber_dana_analisis');
 		$this->loader->add_action('wp_ajax_get_rinc_rka_lokal',  $plugin_public, 'get_rinc_rka_lokal');
+		$this->loader->add_action('wp_ajax_show_skpd_rekening_analisis',  $plugin_public, 'show_skpd_rekening_analisis');
 		
 		add_shortcode('menu_monev',  array($plugin_public, 'menu_monev'));
 		add_shortcode('datassh', array($plugin_public, 'datassh'));
@@ -958,6 +959,7 @@ class Wpsipd
 		add_shortcode('analisis_belanja_sub_kegiatan', array($plugin_public, 'analisis_belanja_sub_kegiatan'));
 		add_shortcode('analisis_belanja_bidang_urusan', array($plugin_public, 'analisis_belanja_bidang_urusan'));
 		add_shortcode('analisis_belanja_sumber_dana', array($plugin_public, 'analisis_belanja_sumber_dana'));
+		add_shortcode('analisis_belanja_rekening', array($plugin_public, 'analisis_belanja_rekening'));
 		add_shortcode('rekap_sumber_dana_per_skpd', array($plugin_public, 'rekap_sumber_dana_per_skpd'));
 		add_shortcode('rekap_sumber_dana_per_program', array($plugin_public, 'rekap_sumber_dana_per_program'));
 		add_shortcode('rekap_sumber_dana_per_kegiatan', array($plugin_public, 'rekap_sumber_dana_per_kegiatan'));
