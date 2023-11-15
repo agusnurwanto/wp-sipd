1face8929711272bece49f0cf3f34af51d54509d
dwisugi <dwisugianto17@gmail.com>
2023-11-15 09:32:50 +0700

    perbaikan bug halaman input renja .2

--- public/partials/wpsipd-public-input-renja.php
+++ public/partials/wpsipd-public-input-renja.php
@@ -190,6 +190,7 @@ $data_all = array(
     'total_pergeseran' => 0,
     'total_usulan_pergeseran' => 0
 );
+$status_pergeseran_renja = '';
 $thead_pergeseran = '';
 $thead_pergeseran1 = 6;
 $thead_pergeseran2 = '';
@@ -246,7 +247,7 @@ foreach ($subkeg as $kk => $sub) {
         order by id ASC
     ", $input['tahun_anggaran'], $sub['kode_sbl']), ARRAY_A);
     
-    $status_pergeseran_renja = (!empty($jadwal_lokal[0]['status_jadwal_pergeseran'])) ? $jadwal_lokal[0]['status_jadwal_pergeseran'] : '';
+    $status_pergeseran_renja = $jadwal_lokal[0]['status_jadwal_pergeseran'];
     $pagu_pergeseran = 0;
     $pagu_usulan_pergeseran = 0;
     $thead_pergeseran = '';
