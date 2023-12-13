806bf0c17a9346e38a65a4c5f964c0766defbb59
Saiful Bangil <saifulbangil@gmail.com>
2023-12-13 16:00:09 +0700

    menampilkan versi wp-sipd pada dokumentasi API

--- public/partials/dokumentasi/wpsipd-public-dokumentasi-api.php
+++ public/partials/dokumentasi/wpsipd-public-dokumentasi-api.php
@@ -580,17 +580,15 @@ body.one-content-column-version .content thead {
             <svg width="34" height="34" viewBox="0 0 100 100"><path class="line line1" d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"></path><path class="line line2" d="M 20,50 H 80"></path><path class="line line3" d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"></path></svg>
         </button>
     </div>
+    <?php $versi = get_option('_wp_sipd_db_version'); 
+    $last_update = get_option('_last_update_sql_migrate');
+    $tgl = str_replace('tabel.sql', '', $last_update);?>
     <div class="mobile-menu-closer"></div>
     <div class="content-menu">
         <div class="content-infos">
-            <div class="info"><b>Version:</b> 1.0.5</div>
-            <div class="info"><b>Last Updated:</b> 15th Sep, 2021</div>
-        </div>
-        <ul>
-            <li class="scroll-to-link active" data-target="content-get-started">
-                <a>Pendahuluan</a>
-            </li>
-            <li class="scroll-to-link" data-target="content-get-skpd">
+            <div class="info"><b>Version:</b> <?php echo $versi; ?></div>
+            <div class="info"><b>Last Updated:</b> <?php echo $tgl; ?></div>
+        </div>target="content-get-skpd">
                 <a>Get SKPD</a>
             </li>
             <li class="scroll-to-link" data-target="content-get-subkeg-by-skpd">
