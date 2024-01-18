863aff4cc67a2516b3cd35e6b3718f511995cdbf
Agus Nurwanto <agusnurwantomuslim@gmail.com>
2024-01-18 15:11:14 +0700

    perbaikan singkron pegawai

--- public/class-wpsipd-public.php
+++ public/class-wpsipd-public.php
@@ -3674,7 +3674,7 @@ class Wpsipd_Public extends Wpsipd_Public_Base_1
 		if (!empty($_POST)) {
 			if (!empty($_POST['api_key']) && $_POST['api_key'] == get_option( '_crb_api_key_extension' )) {
 				if (!empty($_POST['data_user'])) {
-					if(!empty($_POST['sumber']) && $_POST['sumber'] == 'ri'){
+					if(!empty($_POST['type']) && $_POST['type'] == 'ri'){
 						$_POST['data_user'] = json_decode(stripslashes(html_entity_decode($_POST['data_user'])), true);
 					}
 
@@ -3763,7 +3763,7 @@ class Wpsipd_Public extends Wpsipd_Public_Base_1
 		if (!empty($_POST)) {
 			if (!empty($_POST['api_key']) && $_POST['api_key'] == get_option( '_crb_api_key_extension' )) {
 				if (!empty($_POST['data_user'])) {
-					if(!empty($_POST['sumber']) && $_POST['sumber'] == 'ri'){
+					if(!empty($_POST['type']) && $_POST['type'] == 'ri'){
 						$_POST['data_user'] = json_decode(stripslashes(html_entity_decode($_POST['data_user'])), true);
 					}
 					
@@ -3775,7 +3775,8 @@ class Wpsipd_Public extends Wpsipd_Public_Base_1
 							from data_user_penatausahaan 
 							where tahun=%d 								
 								AND idUser=%s
-						", $_POST['tahun_anggaran'], $data_user['idUser']));
+								AND idPegawai=%s
+						", $_POST['tahun_anggaran'], $data_user['idUser'], $data_user['idPegawai']));
 						// print_r($data_user['idUser']);exit;
 						$opsi = array(
 							"idSkpd" => $data_user['idSkpd'],
