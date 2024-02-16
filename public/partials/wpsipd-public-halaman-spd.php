49af26d06b207d79ff0fc513426ef807c392c414
Bagus Alvin <bagusalvin224@gmail.com>
2024-02-16 11:03:29 +0700

    tambah halaman spp

--- public/partials/wpsipd-public-halaman-spd.php
+++ public/partials/wpsipd-public-halaman-spd.php
@@ -1,81 +1,60 @@
 <?php
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-    die;
+if (!defined('WPINC')) {
+	die;
 }
 global $wpdb;
-$input = shortcode_atts( array(
-    'tahun_anggaran' => '2021'
-), $atts );
-$api_key = get_option( '_crb_api_key_extension' );
+$input = shortcode_atts(array(
+	'id_skpd' => '',
+	'tahun_anggaran' => '2022'
+), $atts);
 
-$body = '';
-$_POST['tahun_anggaran'] = $input['tahun_anggaran'];
-$_POST['api_key'] = $api_key;
-$_POST['action'] = '';
-$spd = $this->get_spd(true);
-$no = 0;
-$total_all_simda = 0;
-foreach ($spd['data'] as $val) {
-    // print_r($val); die();
-    $no++;
+if (!empty($input['id_skpd'])) {
+	$sql = $wpdb->prepare("
+		select 
+			* 
+		from data_unit 
+		where tahun_anggaran=%d
+			and id_skpd IN (" . $input['id_skpd'] . ") 
+			and active=1
+		order by id_skpd ASC
+	", $input['tahun_anggaran']);
+} else {
+	$sql = $wpdb->prepare("
+		select 
+			* 
+		from data_unit 
+		where tahun_anggaran=%d
+			and active=1
+		order by id_skpd ASC
+	", $input['tahun_anggaran']);
+}
+$units = $wpdb->get_results($sql, ARRAY_A);
+if (empty($units)) {
+	die('<h1>SKPD tidak ditemukan!</h1>');
+} 
+foreach ($units as $k => $unit) :
 
-    $sql = $wpdb->prepare("
-        SELECT 
-            sum(nilai) as nilai
-        FROM data_spd_rinci 
-        where tahun=%d
-            and no_spd=%s
-        ", 
-        $input['tahun_anggaran'], 
-        $val->no_spd
-    );
-    // print_r($sql); die($wpdb->last_query);
-    $total = $this->simda->CurlSimda(array(
-        'query' => $sql,
-        'debug' => 1
-    ));
-    $total_simda = 0;
-    if(!empty($total[0]->nilai)){
-        $total_simda = $total[0]->nilai;
-    }
-    $nama_skpd = '';
-//     $sql = $wpdb->prepare(" 
-//     	SELECT 
-//            * 
-// 		from data_unit 
-// 		where tahun_anggaran=%d
-// 			and id_skpd =".$input['id_skpd']."
-// 			and active=1
-// 		order by id_skpd ASC
-// 	", $input['tahun_anggaran']);
-		
-// $nama_skpd = $wpdb->get_results($sql, ARRAY_A);
-    if(!empty($val->skpd) && !empty($val->skpd['kode_skpd'])){
-        $nama_skpd = $val->skpd['kode_skpd'].'<br>'.$val->skpd['nama_skpd'];
-        $update = false;
-        $url_skpd = $this->generatePage($title, $input['tahun_anggaran'], $shortcode, $update);
-        $nama_skpd = '<a href="'.$url_skpd.'" target="_blank">'.$nama_skpd.'</a>';
-    }
     $body .= '
         <tr>
             <td class="text-center">'.$no.'</td>
-            <td>'.$nama_skpd.'</td>
+            <td>'.$unit['nama_skpd'].'</td>
             <td class="text-center">'.$val->no_spd.'</td>
             <td class="text-center">'.$val->tgl_spd.'</td>
-            <td class="text-right">'.number_format($total_simda, 2, ',', '.').'</td>
+            <td class="text-right" style="'.$background.'">'.number_format($total_simda, 2, ',', '.').'</td>
         </tr>
     ';
-    $total_all_simda += $total_simda;
-}
+endforeach;
+
 ?>
+
 
 </style>
 <div class="cetak">
     <div style="padding: 10px;">
         <input type="hidden" value="<?php echo get_option( '_crb_api_key_extension' ); ?>" id="api_key">
         <input type="hidden" value="<?php echo $input['tahun_anggaran']; ?>" id="tahun_anggaran">
-        <h1 class="text-center">Data Surat Penyediaan Dana ( SPD )<br>Tahun <?php echo $input['tahun_anggaran']; ?></h1>
+        <h1 class="text-center">Halaman Data SPD Tahun <?php echo $unit['nama_skpd']; ?></h1>
         <table cellpadding="2" cellspacing="0" style="font-family:\'Open Sans\',-apple-system,BlinkMacSystemFont,\'Segoe UI\',sans-serif; border-collapse: collapse; width:100%; overflow-wrap: break-word;" class="table table-bordered">
             <thead id="data_header">
                 <tr>
@@ -91,7 +70,7 @@ foreach ($spd['data'] as $val) {
             </tbody>
             <tfoot>
                 <th colspan="4" class="text-center">Total</th>
-                <th class="text-right"><?php echo number_format($total_all_simda, 2, ',', '.'); ?></th>
+                <th class="text-right" style="<?php echo $background_all; ?>"><?php echo number_format($total_all_simda, 2, ',', '.'); ?></th>
             </tfoot>
         </table>
     </div>
