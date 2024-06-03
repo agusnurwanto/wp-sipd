33002e74b654d514921b3f505fda08089bd1d02a
Saiful Bangil <saifulbangil@gmail.com>
2024-06-03 16:28:34 +0700

    backup data realisasi APBD

--- includes/class-wpsipd.php
+++ includes/class-wpsipd.php
@@ -338,6 +338,9 @@ class Wpsipd
 
 		$this->loader->add_action('wp_ajax_singkron_anggaran_kas',  $plugin_public, 'singkron_anggaran_kas');
 		$this->loader->add_action('wp_ajax_nopriv_singkron_anggaran_kas',  $plugin_public, 'singkron_anggaran_kas');
+
+		$this->loader->add_action('wp_ajax_singkron_realisasi_dashboard',  $plugin_public, 'singkron_realisasi_dashboard');
+		$this->loader->add_action('wp_ajax_nopriv_singkron_realisasi_dashboard',  $plugin_public, 'singkron_realisasi_dashboard');
 		
 		$this->loader->add_action('wp_ajax_singkron_up',  $plugin_public, 'singkron_up');
 		$this->loader->add_action('wp_ajax_nopriv_singkron_up',  $plugin_public, 'singkron_up');
