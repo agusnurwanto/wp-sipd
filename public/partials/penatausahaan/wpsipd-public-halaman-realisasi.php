d8a5d6ca520f74f410a6d4dde6b5cf761b9631d3
Agus Nurwanto <agusnurwantomuslim@gmail.com>
2024-06-19 00:44:11 +0700

    tambah tabel laporan realisasi pendapatan dan pembiayaan

--- public/partials/penatausahaan/wpsipd-public-halaman-realisasi.php
+++ public/partials/penatausahaan/wpsipd-public-halaman-realisasi.php
@@ -50,35 +50,83 @@ $get_realisasi = $wpdb->get_results($wpdb->prepare('
 // die($wpdb->last_query);
 
 $nomor = 0;
+$nomor_pendapatan = 0;
+$nomor_pembiayaan = 0;
 $body = '';
+$body_pendapatan = '';
+$body_pembiayaan = '';
 $total_pagu = 0;
 $total_realisasi = 0;
 $persen_all = '';
+$total_pagu_pendapatan = 0;
+$total_realisasi_pendapatan = 0;
+$persen_all_pendapatan = '';
+$total_pagu_pembiayaan = 0;
+$total_realisasi_pembiayaan = 0;
+$persen_all_pembiayaan = '';
 foreach($get_realisasi as $akun){
-    $nomor++;
     $persen = 0;
     if($akun['nilai'] > 0 && $akun['realisasi'] > 0){
         $persen = round(($akun['realisasi']/$akun['nilai'])*100, 2);
     }
-    $body .= '
-        <tr>
-            <td class="text-center">'.$nomor.'</td>
-            <td class="text-left">'.$akun['kode_skpd'].' '.$akun['nama_skpd'].'</td>
-            <td class="text-left">'.$akun['nama_program'].'</td>
-            <td class="text-left">'.$akun['nama_giat'].'</td>
-            <td class="text-left">'.$akun['nama_sub_giat'].'</td>
-            <td class="text-left">'.$akun['nama_akun'].'</td>
-            <td class="text-right">'.number_format($akun['nilai'],0,",",".").'</td>
-            <td class="text-right">'.number_format($akun['realisasi'],0,",",".").'</td>
-            <td class="text-center">'.$persen.'%</td>
-        </tr>
-    ';
+    if($akun['type'] == 'pendapatan'){
+        $nomor_pendapatan++;
+        $body_pendapatan .= '
+            <tr>
+                <td class="text-center">'.$nomor_pendapatan.'</td>
+                <td class="text-left">'.$akun['kode_skpd'].' '.$akun['nama_skpd'].'</td>
+                <td class="text-left">'.$akun['nama_program'].'</td>
+                <td class="text-left">'.$akun['nama_giat'].'</td>
+                <td class="text-left">'.$akun['nama_sub_giat'].'</td>
+                <td class="text-left">'.$akun['nama_akun'].'</td>
+                <td class="text-right">'.number_format($akun['nilai'],0,",",".").'</td>
+                <td class="text-right">'.number_format($akun['realisasi'],0,",",".").'</td>
+                <td class="text-center">'.$persen.'%</td>
+            </tr>
+        ';
 
-    $total_pagu += $akun['nilai'];
-    $total_realisasi += $akun['realisasi'];
+        $total_pagu_pendapatan += $akun['nilai'];
+        $total_realisasi_pendapatan += $akun['realisasi'];
+    }else if($akun['type'] == 'pembiayaan'){
+        $nomor_pembiayaan++;
+        $body_pembiayaan .= '
+            <tr>
+                <td class="text-center">'.$nomor_pembiayaan.'</td>
+                <td class="text-left">'.$akun['kode_skpd'].' '.$akun['nama_skpd'].'</td>
+                <td class="text-left">'.$akun['nama_akun'].'</td>
+                <td class="text-right">'.number_format($akun['nilai'],0,",",".").'</td>
+                <td class="text-right">'.number_format($akun['realisasi'],0,",",".").'</td>
+                <td class="text-center">'.$persen.'%</td>
+            </tr>
+        ';
+
+        $total_pagu_pembiayaan += $akun['nilai'];
+        $total_realisasi_pembiayaan += $akun['realisasi'];
+    }else{
+        $nomor++;
+        $body .= '
+            <tr>
+                <td class="text-center">'.$nomor.'</td>
+                <td class="text-left">'.$akun['kode_skpd'].' '.$akun['nama_skpd'].'</td>
+                <td class="text-left">'.$akun['nama_akun'].'</td>
+                <td class="text-right">'.number_format($akun['nilai'],0,",",".").'</td>
+                <td class="text-right">'.number_format($akun['realisasi'],0,",",".").'</td>
+                <td class="text-center">'.$persen.'%</td>
+            </tr>
+        ';
+
+        $total_pagu += $akun['nilai'];
+        $total_realisasi += $akun['realisasi'];
+    }
 }
 if($total_pagu > 0 && $total_realisasi > 0){
     $persen_all = round(($total_realisasi/$total_pagu)*100, 2);
+}
+if($total_pagu_pendapatan > 0 && $total_realisasi_pendapatan > 0){
+    $persen_all_pendapatan = round(($total_realisasi_pendapatan/$total_pagu_pendapatan)*100, 2);
+}
+if($total_pagu_pembiayaan > 0 && $total_realisasi_pembiayaan > 0){
+    $persen_all_pembiayaan = round(($total_realisasi_pembiayaan/$total_pagu_pembiayaan)*100, 2);
 }
 ?>
 
@@ -92,6 +140,7 @@ if($total_pagu > 0 && $total_realisasi > 0){
 
 <div class="cetak container-fluid">
     <h1 class="text-center">Halaman Realisasi Per Akun Per Perangkat Daerah<?php echo $nama_skpd; ?><br>Tahun <?php  echo $input['tahun_anggaran'] ?></h1>
+    <h2 class="text-center">Belanja</h2>
     <table class="table table-bordered" id="cetak">
         <thead>
             <tr>
@@ -117,5 +166,53 @@ if($total_pagu > 0 && $total_realisasi > 0){
                 <td class="atas bawah kanan kiri text_tengah"><?php echo $persen_all; ?>%</td>
             </tr>
         </tfoot>
-    </table>      
+    </table>
+    <h2 class="text-center">Pendapatan</h2>
+    <table class="table table-bordered" id="cetak1">
+        <thead>
+            <tr>
+                 <th class="text-center">No</th>
+                 <th class="text-center">Nama SKPD</th>
+                 <th class="text-center">Akun</th>
+                 <th class="text-center">Pagu</th>
+                 <th class="text-center">Realisasi</th>
+                 <th class="text-center">Persen</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php echo $body_pendapatan; ?>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th class="atas bawah kanan kiri text_kanan" colspan="3">Total</th>
+                <td class="atas bawah kanan kiri text_kanan"><?php echo number_format($total_pagu_pendapatan,0,",","."); ?></td>
+                <td class="atas bawah kanan kiri text_kanan"><?php echo number_format($total_realisasi_pendapatan,0,",","."); ?></td>
+                <td class="atas bawah kanan kiri text_tengah"><?php echo $persen_all_pendapatan; ?>%</td>
+            </tr>
+        </tfoot>
+    </table>
+    <h2 class="text-center">Pembiayaan</h2>
+    <table class="table table-bordered" id="cetak2">
+        <thead>
+            <tr>
+                 <th class="text-center">No</th>
+                 <th class="text-center">Nama SKPD</th>
+                 <th class="text-center">Akun</th>
+                 <th class="text-center">Pagu</th>
+                 <th class="text-center">Realisasi</th>
+                 <th class="text-center">Persen</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php echo $body_pembiayaan; ?>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th class="atas bawah kanan kiri text_kanan" colspan="3">Total</th>
+                <td class="atas bawah kanan kiri text_kanan"><?php echo number_format($total_pagu_pembiayaan,0,",","."); ?></td>
+                <td class="atas bawah kanan kiri text_kanan"><?php echo number_format($total_realisasi_pembiayaan,0,",","."); ?></td>
+                <td class="atas bawah kanan kiri text_tengah"><?php echo $persen_all_pembiayaan; ?>%</td>
+            </tr>
+        </tfoot>
+    </table>
 </div>
