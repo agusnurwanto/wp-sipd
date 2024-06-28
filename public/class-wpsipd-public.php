42131dcc14264a73322b3292a96f1ac954648385
Agus Nurwanto <agusnurwantomuslim@gmail.com>
2024-06-28 16:56:11 +0700

    tambah notif jika request ajax gagal

--- public/class-wpsipd-public.php
+++ public/class-wpsipd-public.php
@@ -119,7 +119,8 @@ class Wpsipd_Public extends Wpsipd_Public_Base_1
 		wp_enqueue_script($this->plugin_name . 'datatables', plugin_dir_url(__FILE__) . 'js/datatables.min.js', array('jquery'), $this->version, false);
 		wp_enqueue_script($this->plugin_name . 'chart', plugin_dir_url(__FILE__) . 'js/chart.min.js', array('jquery'), $this->version, false);
 		wp_localize_script($this->plugin_name, 'ajax', array(
-			'url' => admin_url('admin-ajax.php')
+			'url' => admin_url('admin-ajax.php'),
+			'site_url' => site_url()
 		));
 	}
 
