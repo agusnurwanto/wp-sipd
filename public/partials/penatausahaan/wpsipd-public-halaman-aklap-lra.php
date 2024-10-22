52b6b8dc0bfaa13bf7dd979dd2e6c2e57f7c615b
Saiful Bangil <saifulbangil@gmail.com>
2024-10-23 04:20:14 +0700

    set body pendapatan

--- public/partials/penatausahaan/wpsipd-public-halaman-aklap-lra.php
+++ public/partials/penatausahaan/wpsipd-public-halaman-aklap-lra.php
@@ -27,6 +27,139 @@ if ($skpd_result) {
 } else {
     echo 'Data SKPD tidak ditemukan';
 }
+
+function ubah_minus($nilai){
+    if($nilai < 0){
+        $nilai = abs($nilai);
+        return '('.number_format($nilai,2,",",".").')';
+    }else{
+        return number_format($nilai,2,",",".");
+    }
+}
+
+global $wpdb;
+$GLOBALS['pendapatan'] = 0;
+$GLOBALS['belanja'] = 0;
+$GLOBALS['pembiayaan_penerimaan'] = 0;
+$GLOBALS['pembiayaan_pengeluaran'] = 0;
+
+if(!empty($_GET) && !empty($_GET['id_skpd'])){
+    $input['id_skpd'] = $_GET['id_skpd'];
+}
+
+if(!empty($input['id_skpd'])){
+    $sql = $wpdb->prepare("
+        select             
+            kode_akun,
+            nama_akun,
+            sum(nominal) as nominal,
+            sum(realisasi) as realisasi,
+            sum(presentase) as presentase,
+            sum(previous_realisasi) as realisasi_sebelum
+        from aklap_lra_sipd
+        where tahun_anggaran=%d
+            and active=1
+            and id_skpd=%d
+            ".$where_jadwal."
+        group by kode_rekening
+        order by kode_rekening ASC
+    ", $input['tahun_anggaran'], $input['id_skpd']);
+}else{
+    $sql = $wpdb->prepare("
+        select             
+            kode_rekening,
+            nama_rekening,
+            sum(nominal) as nominal,
+            sum(realisasi) as realisasi,
+            sum(presentase) as presentase,
+            sum(previous_realisasi) as realisasi_sebelum
+        from aklap_lra_sipd
+        where tahun_anggaran=%d
+            and active=1
+            ".$where_jadwal."
+        group by kode_rekening
+        order by kode_rekening ASC
+    ", $input['tahun_anggaran']);
+}
+$rek_pendapatan = $wpdb->get_results($sql, ARRAY_A);
+
+$body_pendapatan = generate_body($rek_pendapatan, true, $type, 'Pendapatan', $dari_simda);
+//BELANJA
+if(!empty($input['id_skpd'])){
+    $sql = $wpdb->prepare("
+        select             
+            r.kode_rekening,
+            r.nama_rekening,
+            sum(r.nominal) as total,
+            sum(r.realisasi) as totalmurni,
+            sum(r.presentase) as presentase,
+            sum(r.previous_realisasi) as totalmurni
+        from aklap_lra_sipd
+        inner join data_sub_keg_bl s on s.kode_sbl = r.kode_sbl
+            and s.active = r.active
+            and s.tahun_anggaran = r.tahun_anggaran
+        where r.tahun_anggaran=%d
+            and r.active=1
+            and s.id_sub_skpd=%d
+            ".$where_jadwal_rka."
+            ".$where_sub_keg_bl."
+        group by r.kode_rekening
+        order by r.kode_rekening ASC
+    ", $input['tahun_anggaran'], $input['id_skpd']);
+}else{
+    $sql = $wpdb->prepare("
+        select 
+            0 as realisasi,
+            kode_akun,
+            nama_akun,
+            sum(rincian) as total,
+            sum(rincian_murni) as totalmurni
+        from data_rka".$tabel_history."
+        where tahun_anggaran=%d
+            and active=1
+            ".$where_jadwal."
+        group by kode_akun
+        order by kode_akun ASC
+    ", $input['tahun_anggaran']);
+}
+$rek_belanja = $wpdb->get_results($sql, ARRAY_A);
+
+function generate_body($rek_pendapatan, $baris_kosong=false, $type='murni', $nama_rekening, $dari_simda=0){
+    global $wpdb;
+    global $pendapatan;    
+    global $belanja;    
+    global $pembiayaan_penerimaan;    
+    global $pembiayaan_pengeluaran;
+    
+    $data_pendapatan = array(
+        'data' => array(),
+        'realisasi' => 0,
+        'total' => 0
+    );
+
+    foreach ($rek_pendapatan as $k => $v) {
+        if($dari_simda!=0 && !empty($v['total_simda'])){
+            $v['totalmurni'] = $v['total_simda'];
+        }
+        $rek = explode('.', $v['kode_akun']);
+        $kode_akun = $rek[0];
+        if(!$kode_akun){
+            // print_r($v); die();
+            continue;
+        }
+        if(empty($data_pendapatan['data'][$kode_akun])){
+            $nama_akun = $wpdb->get_results("SELECT nama_akun from data_akun where kode_akun='".$kode_akun."'", ARRAY_A);
+            $data_pendapatan['data'][$kode_akun] = array(
+                'data' => array(),
+                'realisasi' => 0,
+                'nama' => $nama_akun[0]['nama_akun'],
+                'total' => 0,
+                'totalmurni' => 0
+            );
+        }
+        $kode_akun1 = $kode_akun.'.'.$rek[1];
+
+}
 ?>
 <style type="text/css">
     .wrap-table {
